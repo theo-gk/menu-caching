3484b1e61572714a3f5c7c97666ed0e4a5f6fe71
Theo <theodoros.gkitsos@gmail.com>
2022-03-12 02:30:48 +0200

    - fix uninstall + remove extra option

--- uninstall.php
+++ uninstall.php
@@ -11,6 +11,8 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
 
+require_once( WP_PLUGIN_DIR . '/menu-caching/admin/class-menu-caching-admin.php' );
+
 if ( !class_exists( 'Wp_Menu_Caching_Admin' ) ) exit;
 
 $plugin_admin = new Wp_Menu_Caching_Admin( 'menu-caching', '1.0' );
@@ -18,3 +20,4 @@ $plugin_admin->dc_purge_menu_html_transients();
 
 delete_option( 'dc_menu_html_index' );
 delete_option( 'dc_menu_nonces_index' );
+delete_option( 'dc_mc_nocache_menus' );
